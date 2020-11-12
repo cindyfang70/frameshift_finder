12917a6ef0fd0c14f4cd4f8680010ecd64f0b6b5
Cindy Fang <cindyfang70@gmail.com>
2020-11-12 16:10:19 -0500

    removed some print statements and uploading output files

--- new_pat_script.py
+++ new_pat_script.py
@@ -36,15 +36,16 @@ class Organism:
 th_df = pd.read_excel("gpt_dataa.xlsx", sheet_name="gpT")
 tg_df = pd.read_excel("gpt_dataa.xlsx", sheet_name="gpG")
 tm_df = pd.read_excel("gpt_dataa.xlsx", sheet_name="gpH")
-print(len(tm_df), len(tg_df))
+
 th_df.drop_duplicates(subset="Species", keep="first", inplace=True)
 tg_df.drop_duplicates(subset="Species", keep="first", inplace=True)
 tm_df.drop_duplicates(subset="Species", keep="first", inplace=True)
 
 th_df.reset_index(inplace=True)
 tg_df.reset_index(inplace=True)
-tm_df.reset_index(inplace=True)
-print(len(tm_df), len(tg_df))
+
+
+ tm_df.reset_index(inplace=True)
 th_len = len(th_df)
 tg_len = len(tg_df)
 tm_len = len(tm_df)
@@ -75,21 +76,12 @@ for i in range(tm_len):
     tm_organism.tm_refseq = protein_refseq
     tm_organisms.append(tm_organism)
 
-# shared_organisms = []
-# for g_organism in tg_organisms:
-#     for t_organism in th_organisms:
-#         if g_organism.name == t_organism.name:
-#             g_organism.th_refseq = t_organism.th_refseq
-#             shared_organisms.append(g_organism)
-#             continue
-
 shared_by_all = []
 for tg_organism in tg_organisms:
     for tm_organism in tm_organisms:
         if tg_organism.genome_refseq == tm_organism.genome_refseq:
             tg_organism.tm_refseq = tm_organism.tm_refseq
             shared_by_all.append(tg_organism)
-print("shared_by_all: " + str(len(shared_by_all)))
 
 gene_ids = []
 for tg_organism in shared_by_all:
@@ -200,7 +192,6 @@ for tg_organism in shared_by_all:
             g_region_seq = find_actual_DNA_seq(tg_organism.genome_refseq, g_region)
             if tg_organism.iscDNA:
                 g_region_seq = g_region_seq.reverse_complement()
-                # fs_region = (int(g_region[0]) + fs_start, int(g_region[0]) + fs_start + 60)
         except urllib.error.HTTPError:
             pass
 
@@ -210,25 +201,12 @@ for tg_organism in shared_by_all:
         frameshift_regions.append(SeqRecord(frameshift_region[:60],
                                             tg_organism.name + "|" + tg_organism.genome_refseq + " " + "(" + str(fs_region[0]) + ", " + str(fs_region[1]) + ")",
                                             description="Tail Assembly Chaperone-C-Terminal Frameshift"))
-        print(fs_region)
         try:
             found_region = find_actual_DNA_seq(tg_organism.genome_refseq,
                                                fs_region)
         except urllib.error.HTTPError:
             pass
-        # if organism.iscDNA:
-        #     found_region = found_region.reverse_complement()
-        print(found_region)
-        print(tg_organism.extended_tg_sequence)
-        read_start = tg_organism.extended_tg_sequence.find(found_region)
-        print(read_start)
-        onwards = tg_organism.extended_tg_sequence[read_start:]
-        print(onwards)
-        if "TAG" in onwards or "TAA" in onwards or "TGA" in onwards:
-            print("has stop codon")
-            print(tg_organism.extended_tg_sequence.find("TAG"))
-            print(tg_organism.extended_tg_sequence.find("TAA"))
-            print(tg_organism.extended_tg_sequence.find("TGA"))
+
 # alignment = MultipleSeqAlignment(frameshift_regions)
 # print(alignment)
 
